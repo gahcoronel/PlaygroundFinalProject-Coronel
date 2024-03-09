87f7334fc3482ac9304e0d61ac2540a493b868a0
Ariel Coronel <gahcoronel@gmail.com>
2024-03-09 19:02:31 -0300

    Version completa con los test funcionando correctamente

--- AC_Blog/Home/tests.py
+++ AC_Blog/Home/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+import datetime
 
 from Home.models import Articulo
 from django.urls import reverse
@@ -15,55 +16,64 @@ class VerificarRutas(TestCase):
         respuesta = self.client.get(url)
         self.assertEqual(respuesta.status_code, 200)
 
-# class EliminarArticuloTest(TestCase):
-#     def setUp(self):
-#         self.articulo = Articulo.objects.create(
-#             titulo = "Artículo 001", 
-#             subtitulo = "Artículo 001 ST", 
-#             contenido = "These two occurrences are not a coincidence.",
-#             autor = 'testuser',
-#             fecha_creacion = "2024-03-08",
-#             imagen         = "",
-#             )
-#         user = get_user_model()
-#         user.objects.create_user('testuser', password="genericPW1788")
+class EliminarArticuloTest(TestCase):
+    def setUp(self):
+        self.articulo = Articulo.objects.create(
+            titulo = "Artículo 001", 
+            subtitulo = "Artículo 001 ST", 
+            contenido = "These two occurrences are not a coincidence.",
+            autor_id = 1,
+            fecha_creacion = datetime.date(2024, 3, 9),
+            imagen         = "",
+            )
+        user = get_user_model()
+        user.objects.create_user('testuser', password="genericPW1788")
 
-#     def test_setup(self):
-#         """
-#         Verificar que creo adecuadamente la instancia de Articulo
-#         """
-#         self.assertQuerysetEqual(Articulo.objects.filter(titulo__icontains="Artículo 001").values(), 
-#                                  [{'id': 1, 
-#                                    'titulo': 'Artículo 001', 
-#                                    'subtitulo': 'Artículo 001 ST', 
-#                                    'contenido': 'These two occurrences are not a coincidence.',
-                                   
-#                                    }])
+    def test_setup(self):
+        """
+        Verificar que creo adecuadamente la instancia de Articulo
+        """
+        self.assertQuerysetEqual(Articulo.objects.filter(titulo__icontains="Artículo 001").values(), 
+                                 [{'id': 1, 
+                                   'titulo': 'Artículo 001', 
+                                   'subtitulo': 'Artículo 001 ST', 
+                                   'contenido': 'These two occurrences are not a coincidence.',
+                                   'autor_id' : 1,
+                                   'fecha_creacion' : datetime.date(2024, 3, 9),
+                                   'imagen' : "",
+                                   }])
     
-#     def test_login(self):
-#         """
-#         Verificar que se inicie sesión
-#         """
-#         self.assertTrue(self.client.login(username='testuser', password='genericPW1788'))
+    def test_login(self):
+        """
+        Verificar que se inicie sesión
+        """
+        self.assertTrue(self.client.login(username='testuser', password='genericPW1788'))
     
-#     def test_eliminar_articulo(self):
-#         """
-#         Verificar que se elimine articulo al iniciar sesión
-#         """
-#         self.client.login(username='testuser', password='genericPW1788')
-#         url = reverse('ArtDelete', args=[self.articulo.id])
-#         respuesta = self.client.post(url)
-#         self.assertEqual(respuesta.status_code, 302)
-#         self.assertQuerysetEqual(models.Articulo.objects.filter(titulo__icontains="Artículo 001", subtitulo__icontains="Artículo 001 ST"), 
-#                                  [])
+    def test_eliminar_articulo(self):
+        """
+        Verificar que se elimine articulo al iniciar sesión
+        """
+        self.client.login(username='testuser', password='genericPW1788')
+        url = reverse('ArtDelete', args=[self.articulo.id])
+        respuesta = self.client.post(url)
+        self.assertEqual(respuesta.status_code, 302)
+        self.assertQuerysetEqual(Articulo.objects.filter(titulo__icontains="Artículo 001", subtitulo__icontains="Artículo 001 ST"), 
+                                 [])
 
-#     def test_no_eliminar_estudiante(self):
-#         """
-#         Verificar que NO se elimine estudiante sin iniciar sesión
-#         """
-#         url = reverse('ArtDelete', args=[self.articulo.id])
-#         respuesta = self.client.post(url)
-#         self.assertEqual(respuesta.status_code, 302)
-#         self.assertQuerysetEqual(models.Articulo.objects.filter(titulo__icontains="Artículo 001", subtitulo__icontains="Artículo 001 ST").values(), 
-#                                  [{'titulo': 'Artículo 001', 'subtitulo': 'Artículo 001 ST', 'id': 1, 'contenido': 'These two occurrences are not a coincidence.'}])
+    def test_no_eliminar_estudiante(self):
+        """
+        Verificar que NO se elimine estudiante sin iniciar sesión
+        """
+        url = reverse('ArtDelete', args=[self.articulo.id])
+        respuesta = self.client.post(url)
+        self.assertEqual(respuesta.status_code, 302)
+        self.assertQuerysetEqual(Articulo.objects.filter(titulo__icontains="Artículo 001", subtitulo__icontains="Artículo 001 ST").values(), 
+                                 [{'id': 1, 
+                                   'titulo': 'Artículo 001', 
+                                   'subtitulo': 'Artículo 001 ST', 
+                                   'contenido': 'These two occurrences are not a coincidence.',
+                                   'autor_id' : 1,
+                                   'fecha_creacion' : datetime.date(2024, 3, 9),
+                                   'imagen' : "",
+                                   }])
     
